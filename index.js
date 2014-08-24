17c5abe878c59dd43448a7e9f3892017b238167b
Mattias Runge <mattias@runge.se>
2014-08-24 10:25:25 +0200

    Stuff

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-'use strict';
+/* global define, require, document */
 
 require.config({
-  baseUrl: "./node_modules/mfw",
   paths: {
     text        : "node_modules/text/text",
-    tpl         : "node_modules/requirejs-tpl/lib/tpl",
     jquery      : "node_modules/jquery/dist/jquery.min",
-    emitter     : "node_modules/tiny-emitter/dist/tinyemitter.min",
-    knockout    : "node_modules/knockout/build/output/knockout-latest",
+    tpl         : "node_modules/requirejs-tpl/lib/tpl",
+    q           : "node_modules/q/q",
+    
+    bootstrap   : "modules/bootstrap/js/bootstrap.min",
+    
+    css         : "modules/css",
 
-    ko          : "lib/ko.extensions",
-    core        : "lib/core",
-    root        : "../../"
+    knockout    : "lib/ko.extensions",
+    core        : "lib/core"
+  },
+  shim: {
+    "bootstrap": {
+      deps: ["jquery"],
+      exports: "jQuery"
+    },
+    "jquery-cookie": {
+      deps: ["jquery"],
+      exports: "jQuery"
+    },
+    "typeahead": {
+      deps: ["jquery"],
+      exports: "jQuery"
+    },
   }
 });
 
 define([
   "core",
   "jquery",
-  "root/main/Main"
+  "main/Main"
 ], function(core, $, Main) {
+  "use strict";
+
   core.com.on("SetTitle", function(title) {
     document.title = title;
   });
 
-  core.com.on("NavigateTo", function(path) {
-    document.location.hash = "#" + path;
-  });
-
-  new Main().init();
-
   $(function() {
+    var main = new Main();
+    
+    main.init();
+    
     core.router.process();
   });
 });
